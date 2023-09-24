bf21a459d28ebc9195a4bf850f76c5448629fb5b
QcJPG <138160427+QCJPG@users.noreply.github.com>
2023-09-23 21:06:04 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,3 +46,5 @@ streamlit.dataframe(my_data_rows)
 # Allow the end user to add a fruit to the list
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
 streamlit.write('The user entered ', add_my_fruit)
+insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values ('from streamlit');
+select * from PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST;
